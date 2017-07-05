19421d655eb100b895144b4487b24acd97083a21
Shubhradeep Majumdar <smaj125@gmail.com>
2017-07-06 00:18:26 +0530

    shifted from Perl to Java environment

--- readwrite.R
+++ readwrite.R
@@ -12,19 +12,19 @@ readwrite <- function(filepath, writepath, sheet = 1, skip = 0, read = TRUE, wri
     } else if (stringr::str_sub(filepath,(stringr::str_length(filepath)-7),-1) == "sas7bdat"){
       haven::read_sas(filepath)
     }
-  } if (write){
+  } else if (write){
     if (stringr::str_sub(writepath,(stringr::str_length(writepath)-2),-1) == "csv"){
       write.csv(x = filepath, file = writepath)
     } else if (stringr::str_sub(writepath,(stringr::str_length(writepath)-2),-1) == "xls" ||
                stringr::str_sub(writepath,(stringr::str_length(writepath)-3),-1) == "xlsx") {
-      WriteXLS::write_excel(filepath, writepath, sheet = sheet)
+      xlsx::write.xlsx(filepath, writepath, sheetName = sheet, row.names = FALSE)
     }
   }
 }
 
 # Import/Export file function by SAM
-# Can be used to import/export any file, except SPSS files.
+# Can be used to import any file, except SPSS files.
 # Who the FUCK uses SPSS anyaway?!
-# Dependencies: "stringr", "readxl", "haven", "readr", "WriteXLS"
+# Dependencies: "stringr", "readxl", "haven", "readr", "xlsx"
 # "haven" (with dependency on readr) used for importing SAS & STATA files
-# For exporting (write = TRUE) filepath is the dataset to be exported.
+# For exporting (write = TRUE) filepath is the dataset to be exported. And, sheet should be in double quotes. eg - "sheet1"
